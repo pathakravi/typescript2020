9724a203a55b7172f8aca514427abb6bf2c5af74
pathakravi <ravipathak0804@gmail.com>
2019-11-17 00:14:09 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -3,4 +3,14 @@ import './style.css';
 
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('demo1');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+appDiv.innerHTML = `<h1>Welcome to TypeScript !</h1>`;
+
+//Basic types In typescript
+//boolean
+let isDone: boolean = false;
+
+//number, All numbers in typescript are floating point values
+let decimal: number = 6;
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o744;
